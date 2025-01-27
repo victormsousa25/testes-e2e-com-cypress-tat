5cf93098caf49a8dac564bacb3c52dbad0578ce2
unknown <victor.sousa@pathfind.com>
2025-01-27 14:39:55 -0300

    Create e2e tests with cypress

---
+++ cypress.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  chromeWebSecurity: false,
+  e2e: {
+    baseurl: 'https://notes-serverless-app.com',
+    env: {
+      viewportWidthBreakpoint: 768,
+    },
+    
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+})
