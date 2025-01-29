26634d734cd6d1f1c1a35d367e3ef66bdd97b87d
unknown <victor.sousa@pathfind.com>
2025-01-29 09:25:45 -0300

    Configure cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -7,9 +7,7 @@ module.exports = defineConfig({
     env: {
       viewportWidthBreakpoint: 768,
     },
-    
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
   },
+  projectId: "gxyqb2"
+
 })
