34985507d0733bb455c71c336afec9962a8cd479
unknown <victor.sousa@pathfind.com>
2025-02-03 10:33:46 -0300

    Create e2e tests with cypress

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,6 @@ import pluginJs from "@eslint/js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
   {languageOptions: { globals: {...globals.browser, ...globals.node} }},
   pluginJs.configs.recommended,
 ];
